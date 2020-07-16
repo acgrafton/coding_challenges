1b4e3ec6cdd50e3a6321dbf1749e73dad925c811
Angela Grafton <ayeung06@gmail.com>
2020-07-16 00:40:28 -0600

    sol#1 edge cases

--- merging_meeting_times.py
+++ merging_meeting_times.py
@@ -3,8 +3,20 @@ def merge_ranges(mtg_time_ranges):
 
     >>> merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
     [(0, 1), (3, 8), (9, 12)]
+
+    >>> merge_ranges([(1, 2), (2, 3)])
+    [(1, 3)]
+
+    >>> merge_ranges([(1, 5), (2, 3)])
+    [(1, 5)]
+
+    >>> merge_ranges([(1, 10), (2, 6), (3, 5), (7, 9)])
+    [(1, 10)]
     
     """
+
+    if len(mtg_time_ranges) <= 1:
+        return mtg_time_ranges
 
     mtg_time_ranges.sort(reverse=True)
 
@@ -18,11 +30,15 @@ def merge_ranges(mtg_time_ranges):
         
         to_check = mtg_time_ranges.pop()
 
+        #If to_check start time does not overlap with the current end time, add the current set of start, end to the merged.
         if to_check[0] > end:
             merged.append((start, end))
             start = to_check[0]
             end = to_check[1]
         
+        elif to_check[1] < end:
+            continue 
+
         else:
             end = to_check[1]
 
