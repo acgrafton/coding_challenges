f51a6879614dd3772b35701ea29d9b1da28e72f2
Angela Grafton <ayeung06@gmail.com>
2020-08-23 22:12:10 -0600

    two-sum better solution

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -1,17 +1,10 @@
 def twoSum(nums, target):
     
-
-    
-    sorted_nums = sorted(nums[:target])
-    
-    index = 0
-    print(sorted_nums)
-    for num in sorted_nums:
-        find_num = 0 if target == 0 else target - num
-        print(find_num)
-        if (find_num) in sorted_nums[index+1:]:
-            i = nums.index(num)
-            print(i)
-            return [i, nums.index(find_num, i+1)]
-        index += 1
+    seen_dict = {}
+        
+    for i, num in enumerate(nums):
+        to_find = target - num
+        if to_find in seen_dict:
+            return (seen_dict[to_find], i)
+        seen_dict[num] = i
             
